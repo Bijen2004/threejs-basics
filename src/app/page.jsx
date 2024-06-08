673329670db6099a13e2cb874435626e9c99c3ec
Bijen Shrestha <shrbijen31@gmail.com>
2024-06-08 11:02:08 +0545

    light and its types

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 'use client'
+import { MeshWobbleMaterial, OrbitControls, useHelper } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import React, { useRef, useState } from 'react'
+import {DirectionalLightHelper} from 'three'
 
 const Cube = ({position,size,color})=>{
   const ref = useRef()
@@ -55,39 +57,52 @@ const Torus=({position,size,color})=>{
 
 const TorusKnot=({position,size,color})=>{
   const ref = useRef()
-  useFrame((state,delta)=>{
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += delta*2
-    ref.current.position.z = Math.sin(state.clock.elapsedTime)*2
-  })
+  // useFrame((state,delta)=>{
+  //   ref.current.rotation.x += delta
+  //   ref.current.rotation.y += delta*2
+  //   ref.current.position.z = Math.sin(state.clock.elapsedTime)*2
+  // })
   return(
     <mesh position={position} ref={ref}>
       <torusKnotGeometry args={size}/>
-      <meshStandardMaterial color={color}/>
+      <MeshWobbleMaterial factor={10} speed={2}/>
     </mesh>
   )
+}
+
+const Scene = ()=>{
+
+  const directionalLightRef = useRef()
+  useHelper(directionalLightRef,DirectionalLightHelper,0.5,'white')
+
+  return(
+    <>
+    <directionalLight position={[-3,1,2]} intensity={0.5} ref={directionalLightRef}/>
+         <ambientLight intensity={0.1}/>
+ 
+       {/* <group position={[0,-1,0]}>
+         <Cube position={[1,0,0]} color={'green'} size={[1,1,1]}/>
+         <Cube position={[-1,0,0]} color={'hotpink'} size={[1,1,1]}/>
+         <Cube position={[1,2,0]} color={'yellow'} size={[1,1,1]}/>
+         <Cube position={[-1,2,0]} color={'blue'} size={[1,1,1]}/>
+       </group>     */}
+ 
+       {/* <Cube position={[0,0,0]} size={[1,1,1]} color={'orange'}/> */}
+         
+         {/* <Sphere position={[0,0,0]} size={[1,30,30]}/> */}
+         {/* <Torus position={[2,0,0]} size={[0.8,0.2,30,30]} color={'blue'}/> */}
+         <TorusKnot position={[0,0,0]} size={[1,0.1,1000,50]} color={'yellow'}/>
+         <OrbitControls enableZoom={false}/>
+   </>
+  )
+ 
 }
 
 const page = () => {
   return (
     <div>
       <Canvas>
-        <directionalLight position={[0,0,2]} intensity={0.5}/>
-        <ambientLight intensity={0.1}/>
-
-      {/* <group position={[0,-1,0]}>
-        <Cube position={[1,0,0]} color={'green'} size={[1,1,1]}/>
-        <Cube position={[-1,0,0]} color={'hotpink'} size={[1,1,1]}/>
-        <Cube position={[1,2,0]} color={'yellow'} size={[1,1,1]}/>
-        <Cube position={[-1,2,0]} color={'blue'} size={[1,1,1]}/>
-      </group>     */}
-
-      {/* <Cube position={[0,0,0]} size={[1,1,1]} color={'orange'}/> */}
-        
-        <Sphere position={[0,0,0]} size={[1,30,30]}/>
-        {/* <Torus position={[2,0,0]} size={[0.8,0.2,30,30]} color={'blue'}/>
-        <TorusKnot position={[-2,0,0]} size={[0.5,0.1,1000,50]} color={'yellow'}/> */}
-
+       <Scene />
       </Canvas>
     </div>
   )
